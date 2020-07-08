27b8babb4cefa13937b9e406269d9e856eb410af
kooooichi24 <ex.kouichi1224@gmail.com>
2020-07-08 20:09:41 +0900

    login動作に関して、vuexのstateから値を取得して描画できた

---
+++ src/components/Navigation.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <Authentication />
+  </div>
+</template>
+
+<script>
+import Authentication from '@/components/Authentication';
+
+export default {
+  name: 'Navigation',
+  components: {
+    Authentication,
+  },
+};
+</script>
+
+<style>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>
+      <p>
+        <strong>{{ user.displayName }} </strong> <small>{{ user.email }} </small>
+        <br>
+        {{ user.uid }}
+      </p>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  computed: {
+    user() {
+      return this.$store.getters.user;
+    }
+  }
+};
+</script>
